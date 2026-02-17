849bae81c7e35cd224eee640f76f97f19f3a60fc
Dev Doshi <dev.doshi@campus.tu-berlin.de>
2026-02-17 20:16:38 +0100

    fix: include node_modules in packaged app and add CSP
    - Remove node_modules exclusion from files array that was stripping all
      runtime dependencies (electron-serve, electron-updater) from build
    - electron-builder auto-includes production deps by default
    - Add Content-Security-Policy header to fix Electron security warning

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, dialog, shell } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog, shell, session } = require("electron");
 const path = require("path");
 const serve = require("electron-serve");
 const { autoUpdater } = require("electron-updater");
@@ -101,6 +101,18 @@ async function createWindow() {
   // Graceful show
   mainWindow.once("ready-to-show", () => {
     mainWindow.show();
+  });
+
+  // Set Content Security Policy
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        "Content-Security-Policy": [
+          "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' data:; connect-src 'self' https://api.github.com",
+        ],
+      },
+    });
   });
 
   // Open external links in default browser
